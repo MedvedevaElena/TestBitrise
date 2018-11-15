de91addba8ae54106387cacd3f7eba002b46e928
femida <proskura.e@gmail.com>
2018-11-15 23:54:56 +0300

    ghfh

---
+++ TestAutoDeploy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestAutoDeploy
+//
+//  Created by Елена Медведева on 15.11.2018.
+//  Copyright © 2018 Елена Медведева. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
